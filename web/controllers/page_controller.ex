79040676e747958532b443b4c1e36199c9ae7bcc
Dean Chouinard <deanchouinard@gmail.com>
2016-04-22 23:27:54 -0400

    show data from session

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Minishop.PageController do
   use Minishop.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    cart = get_session(conn, :cart)
+    render conn, "index.html", cart: cart
   end
 end
--- web/controllers/session.ex
+++ web/controllers/session.ex
@@ -4,7 +4,7 @@ defmodule Minishop.SessionController do
 
   def add(conn, %{"session" => %{"product" => prod}}) do
     conn = assign(conn, :product, prod)
-    conn = put_session(conn, :product, prod)
+    conn = put_session(conn, :cart, prod)
     redirect(conn, to: page_path(conn, :index))
 
   end

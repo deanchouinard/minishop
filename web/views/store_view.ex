8923d5390210a07cd79d08e3cb0c6a929fb4fd92
Dean Chouinard <deanchouinard@gmail.com>
2016-05-05 08:35:15 -0400

    display cart total

--- web/views/store_view.ex
+++ web/views/store_view.ex
@@ -4,9 +4,14 @@ defmodule Minishop.StoreView do
 #  use  Monitized.Money
 
   alias Monetized.Money
+  alias Decimal, as: D
 
   def decimal_to_currency(val) do
     Money.to_string(Money.make(val, [currency: "USD"]), [currency_symbol: true])
   end
 
+  def cart_total(cart) do
+    Enum.reduce(cart, D.new(0), fn(item, total) -> 
+    total = D.add(total, D.mult(D.new(item.qty), D.new(item.price))) end)
+  end
 end

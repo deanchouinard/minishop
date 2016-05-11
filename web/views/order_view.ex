9be3b3d8030a7d64baa489ac3a493eb2b97d2c61
Dean Chouinard <deanchouinard@gmail.com>
2016-05-10 23:14:50 -0400

    order display

--- web/views/order_view.ex
+++ web/views/order_view.ex
@@ -1,3 +1,14 @@
 defmodule Minishop.OrderView do
   use Minishop.Web, :view
+
+  import Ecto.Query
+  alias Minishop.Repo
+  alias Minishop.Pay_Type
+
+  def display_pay_type(id) do
+    Repo.one from p in Pay_Type,
+      where: p.id == ^id,
+      select: p.description
+  end
+
 end

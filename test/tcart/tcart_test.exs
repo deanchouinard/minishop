0b93d31165df14a0091cc13894e888779ad213ba
Dean Chouinard <deanchouinard@gmail.com>
2016-05-31 23:22:38 -0400

    separated cart and server modules into their own files

--- test/tcart/tcart_test.exs
+++ test/tcart/tcart_test.exs
@@ -6,27 +6,27 @@ defmodule TcartTest do
   end
 
   test "cart_server" do
-    {:ok, pid} = CartServer.start
-    CartServer.add_item(pid, %{date: {2015, 1, 1}, title: "Dinner"})
-    CartServer.add_item(pid, %{date: {2015, 1, 2}, title: "Dentist"})
-    CartServer.add_item(pid, %{date: {2015, 1, 2}, title: "Movie"})
+    {:ok, pid} = Minishop.Server.start
+    Minishop.Server.add_item(pid, %{date: {2015, 1, 1}, title: "Dinner"})
+    Minishop.Server.add_item(pid, %{date: {2015, 1, 2}, title: "Dentist"})
+    Minishop.Server.add_item(pid, %{date: {2015, 1, 2}, title: "Movie"})
 
     assert [%{date: {2015, 1, 1}, id: 1, title: "Dinner"}] ==
-      CartServer.items(pid, {2015, 1, 1})
+      Minishop.Server.items(pid, {2015, 1, 1})
 
-    IO.inspect CartServer.list(pid)
+    IO.inspect Minishop.Server.list(pid)
   end
 
   test "item server" do
-    {:ok, pid} = CartServer.start
-    CartServer.add_item(pid, %{qty: 1, product_id: 4})
-    CartServer.add_item(pid, %{qty: 2, product_id: 5})
-    CartServer.add_item(pid, %{qty: 1, product_id: 8})
+    {:ok, pid} = Minishop.Server.start
+    Minishop.Server.add_item(pid, %{qty: 1, product_id: 4})
+    Minishop.Server.add_item(pid, %{qty: 2, product_id: 5})
+    Minishop.Server.add_item(pid, %{qty: 1, product_id: 8})
 
     assert [%{qty: 1, id: 1, product_id: 4}] ==
-      CartServer.line_items(pid, 4)
+      Minishop.Server.line_items(pid, 4)
 
-    IO.inspect CartServer.list(pid)
+    IO.inspect Minishop.Server.list(pid)
   end
 end
 

c056464c2768efdaf385ce84edf483614233bb1c
Dean Chouinard <deanchouinard@gmail.com>
2016-09-28 08:29:28 -0400

    refactor cart plug

--- web/controllers/cart.ex
+++ web/controllers/cart.ex
@@ -12,48 +12,17 @@ defmodule Minishop.Cart do
   end
 
   def call(conn, repo) do
-    # case cart = get_session(conn, :cart) do
-    #   nil ->
-    #     cart = []
-        # cart = [%{prod_id: 12}]
-#        cart = [%Minishop.Cart.Item{}]
-    # _ ->
-      #        cart
-      #    end
-
-    #    cart_id = "100"
-    # conn = put_session(conn, :cart, cart)
-    # conn = assign(conn, :cart, cart)
-    #  IO.inspect(conn)
-    # IO.inspect(cart)
-    #
-    # skey = get_session(conn, :cookies)
-    # IO.puts "skey"
-    # IO.inspect skey
-    # IO.puts "cookies"
-    # IO.inspect(conn.cookies["_minishop_key"])
-    #    IO.inspect(conn)
-
     cart_key = conn.cookies["cart_key"]
-    cart_key = case cart_key do
+    {conn, cart_key} = case cart_key do
       nil -> token = to_string(random(100000))
-            IO.puts "rtoken #{token}"
-            cart_key = Phoenix.Token.sign(conn, "cart_key", token)
-            #            IO.puts "new cart key #{cart_key}"
-      # "" -> token = to_string(random(100000))
-      #       IO.puts "rtoken #{token}"
-      #       cart_key = Phoenix.Token.sign(conn, "cart_key", token)
-      #       conn = Plug.Conn.put_resp_cookie(conn, "cart_key", cart_key)
-      #       IO.puts "new cart key #{cart_key}"
-      _ -> cart_key
+        IO.puts "rtoken #{token}"
+        cart_key = Phoenix.Token.sign(conn, "cart_key", token)
+        conn = Plug.Conn.put_resp_cookie(conn, "cart_key", cart_key)
+        {conn, cart_key}
+      _ -> {conn, cart_key}
     end
 
-    #TODO: this is called unessisarily if the cart_key existed
-    conn = Plug.Conn.put_resp_cookie(conn, "cart_key", cart_key)
-    #    IO.inspect conn
-    IO.inspect cart_key
     {:ok, cart_key} = Phoenix.Token.verify(conn, "cart_key", cart_key)
-    # IO.inspect cart_key
     conn = assign(conn, :cart_key, cart_key)
     cart_pid = Tcart.Cache.server_process(cart_key)
     cart = Tcart.Server.list(cart_pid)
